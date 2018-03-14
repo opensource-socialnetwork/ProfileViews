77e54aabbb0eaaedb822077608998b484d723cdf
Arsalan Shah <manactric@gmail.com>
2018-03-14 19:30:13 +0500

    1.0

---
+++ locale/ossn.en.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   (softlab24.com).ossn
+ * @author    OSSN Core Team <info@softlab24.com>
+ * @copyright 2014-2017 SOFTLAB24 LIMITED
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+$en = array(
+	'profileviews' => 'Profile Views',
+);
+ossn_register_languages('en', $en); 
